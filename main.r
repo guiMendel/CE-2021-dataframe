baec5f93f02b4f10eef33b568927fa6bab7ae166
guimendel <guimendeln@gmail.com>
2021-10-05 17:34:49 -0300

    ajustes finais

--- main.r
+++ main.r
@@ -1,4 +1,5 @@
 library("bib2df")
+library("dplyr")
 
 columnsToKeep <- c(
   "AUTHOR",
@@ -24,6 +25,16 @@ filterColumns <- function(df) {
   return(df[columnsToKeep])
 }
 
+# Returns dataframe with only the selected language
+filterLanguage <- function(recs, language) {
+  newDF <- filter(recs, LANGUAGE == language)
+}
+
+# Returns dataframe without the selected language
+removeLanguage <- function(recs, language) {
+  newDF <- filter(recs, LANGUAGE != language)
+}
+
 readAllRecs <- function() {
   # Start with the base one
   recs <- filterColumns(bib2df("savedrecs.bib"))
@@ -32,17 +43,37 @@ readAllRecs <- function() {
   tryCatch(
     {
       for (index in 1:9999) {
+        print(index)
         fileRecs <- bib2df(sprintf("savedrecs (%d).bib", index))
+
+        # Make sure necessary columns are there
+        for (column in columnsToKeep) {
+          if(!column %in% colnames(fileRecs)) {
+            fileRecs[, column] <- NA
+          }
+        }
+
         recs <- rbind(recs, filterColumns(fileRecs))
       }
     },
     error=function(cond) {
+      print('Stopped. Message:')
+      print(cond)
       return(NA)
     },
     warning=function(cond) {
+      print('Stopped. Message:')
+      print(cond)
       return(NA)
     }
   )
+  
+  # Ensure numeric columns
+  recs <- transform(recs, YEAR = as.numeric(YEAR))
+  recs <- transform(recs, NUMBER.OF.CITED.REFERENCES = as.numeric(NUMBER.OF.CITED.REFERENCES))
+  recs <- transform(recs, TIMES.CITED = as.numeric(TIMES.CITED))
+  recs <- transform(recs, USAGE.COUNT.LAST.180.DAYS = as.numeric(USAGE.COUNT.LAST.180.DAYS))
+  recs <- transform(recs, USAGE.COUNT.SINCE.2013 = as.numeric(USAGE.COUNT.SINCE.2013))
   
   return(recs)
 }
